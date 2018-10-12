29c8e69f11a8c1222ae0df9970ce1a71c9cf1652
samundrak <samundra.khatri@javra.com>
2018-10-12 18:33:00 +0545

    Place mapping wip

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 var inspect = require('eyes').inspector({ maxLength: 20000 });
 const path = require('path');
 var pdf_extract = require('pdf-extract');
+const utils = require('./utils');
 
 var absolute_path_to_pdf = path.join(__dirname, 'Bhadadar.pdf');
 var options = {
@@ -14,8 +15,7 @@ var processor = pdf_extract(absolute_path_to_pdf, options, function(err) {
   }
 });
 processor.on('complete', function(data) {
-  const vadas = data.text_pages.map(textPage => {
-    data.text_pages;
+  const vadas = data.text_pages.map((textPage, index) => {
     // const textPage = data.text_pages[1];
     const ankas = ['०', '१', '२', '३', '४', '५', '६', '७', '८', '९'];
     console.log(textPage);
@@ -27,36 +27,58 @@ processor.on('complete', function(data) {
         return haveAnka;
       })
       .map(item => item.replace(/\n/, ' '));
-    console.log(sanitizedVadadar);
-    const createVada = sanitizedVadadar.map(item => {
-      console.log(
-        item
+    const createVada = sanitizedVadadar
+      .map(item => {
+        const vada = item
           .trim()
           .replace(/\s(\s|\s\s)(\s)*/g, 'X')
-          .split('X'),
-      );
-      const vada = item
-        .trim()
-        .replace(/\s(\s|\s\s)(\s)*/g, 'X')
-        .split('X');
-      if (vada.length < 3) return undefined;
-      const crackDestiny = vada[1].split('–');
-      return {
-        no: vada[0],
-        route: {
-          start: crackDestiny[0],
-          end: crackDestiny[1],
-        },
-        distanceInKm: vada[2],
-        fair: vada[3],
-        oldFair: vada[4],
-      };
-    });
+          .split('X');
+        if (vada.length < 3) return undefined;
+        const crackDestiny = vada[1].split('–');
+        return {
+          route: {
+            start: crackDestiny[0],
+            middle: crackDestiny.slice(1, crackDestiny.length - 1),
+            end: crackDestiny[crackDestiny.length - 1],
+          },
+          distanceInKm: {
+            np: vada[2],
+            en: utils.replaceAnkaWithNumber(vada[2]),
+          },
+
+          fair: {
+            np: vada[3],
+            en: utils.replaceAnkaWithNumber(vada[3]),
+          },
+          oldFair: vada[4],
+        };
+      })
+      .filter(item => item);
+    fs.writeFileSync(`data/vadadar_${index}.json`, JSON.stringify(createVada));
     return createVada;
-    //   console.log(createVada);
   });
-  console.log(vadas)
-  fs.writeFileSync('vadadar.json', JSON.stringify(vadas))
+
+  let t = {};
+  vadas.forEach((n, index) => {
+    t[index] = Object.assign(
+      {},
+      n.reduce((o, i) => {
+        const routes = Object.values(i.route);
+        const obj = {};
+        routes.forEach(element => {
+          if (Array.isArray(element)) {
+            element.forEach(e => {
+              obj[e] = { en: '', np: e };
+            });
+          } else {
+            obj[element] = { en: '', np: element };
+          }
+        });
+        return Object.assign({}, obj, o);
+      }, {}),
+    );
+  });
+  fs.writeFileSync(`dats.json`, JSON.stringify(t));
   //   inspect(data.text_pages, 'extracted text pages');
   //   callback(null, text_pages);
 });
---
+++ utils.js
@@ -0,0 +1,18 @@
+const ankas = ['०', '१', '२', '३', '४', '५', '६', '७', '८', '९'];
+
+function replaceNumberWithAnka(angrejiNumber) {
+  return String(angrejiNumber)
+    .split('')
+    .map(number => anka[number])
+    .join('');
+}
+function replaceAnkaWithNumber(nums) {
+  return nums
+    .split('')
+    .map(anka => ankas.indexOf(anka))
+    .join('');
+}
+module.exports = {
+  replaceAnkaWithNumber,
+  replaceNumberWithAnka,
+};
