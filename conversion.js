4870cd42a8e98e3b533713eb9fe3906666c7746c
samundrak <samundrak@yahoo.com>
2019-03-21 23:53:46 +0545

    WIP translating nepali place to english

---
+++ conversion.js
@@ -0,0 +1,14 @@
+// This CLI will ask for translation of every nepali place and geo coordinates
+const json = require('./dats.json');
+const flatmap = require('lodash.flatmap');
+var inquirer = require('inquirer');
+const fs = require('fs');
+
+const places = Object.values(json);
+const values = [...new Set(flatmap(places.map((item) => Object.keys(item))))];
+
+const questions = [];
+values.forEach((place, index) => {
+  questions.push(`${place} - `);
+});
+fs.writeFileSync('placenames.txt', questions.join('\n'));
